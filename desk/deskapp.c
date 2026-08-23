0dbe00929ca4e3c85a4f9cca6ab934016eb37ad7
bbbradsmith <bbbradsmith@users.noreply.github.com>
2026-08-23 04:37:05 -0400

    revert inf_rev_level to static

--- desk/deskapp.c
+++ desk/deskapp.c
@@ -141,7 +141,7 @@
 #define MAX_SIZE_INF_LINE   (MAXPATHLEN+100)    /* conservative */
 
 
-GLOBAL WORD     inf_rev_level;  /* revision level of current EMUDESK.INF */
+static WORD     inf_rev_level;  /* revision level of current EMUDESK.INF */
 
 static char     *atextptr;      /* current pointer within ANODE text buffer */
 
